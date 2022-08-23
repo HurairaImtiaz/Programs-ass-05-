2920c1253cce707fc03091e4ad6b250c8000c5ce
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-23 11:03:21 +0500

    program 2
    Write a ts program to find the maximum between three numbers.

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
 var num1:number= 10;
-var num2:number= 15;
-if (num1>num2)
+var num2:number= 25;
+var num3:number= 20;
+if (num1>num2 &&  num1>num3)
 {
     console.log("the greater number is =", num1)
 }
-else
+else if (num2>num1 &&  num2>num3)
 {
     console.log("the greater number is =", num2) 
 }
-console.log("congratulations");
+else
+{
+    console.log("the greater number is =" ,num3)
+}
