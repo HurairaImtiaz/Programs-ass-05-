3173bbc1628e7f5cf06a2423d176cce9f58ac16b
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-23 11:02:18 +0500

    program 1
    Write a ts program to find the maximum between two numbers.

---
+++ app.ts
@@ -0,0 +1,11 @@
+var num1:number= 10;
+var num2:number= 15;
+if (num1>num2)
+{
+    console.log("the greater number is =", num1)
+}
+else
+{
+    console.log("the greater number is =", num2) 
+}
+console.log("congratulations");
